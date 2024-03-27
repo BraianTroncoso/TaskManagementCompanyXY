1c1bc9737ddd9b28186aac30443379f0d72b2e70
Braian <troncosobraianaxel@gmail.com>
2024-03-27 04:16:18 -0300

    [UPDATE] validations user name in home

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -10,16 +10,14 @@
 {{ messageAlert }}
   </div>
 </div>
-<div v-if="messageUser" class="flex items-center p-4 mb-4 text-sm text-green-800 border border-green-300 rounded-lg bg-green-50 dark:bg-gray-800 dark:text-green-400 dark:border-green-800 max-w-md mx-auto" role="alert">
-  <svg class="flex-shrink-0 inline w-4 h-4 me-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-    <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z"/>
-  </svg>
+<div v-if="messageUser" class="flex items-center p-4 mb-4 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-800 max-w-md mx-auto" role="alert">
+  
   <span class="sr-only">Info</span>
-  <div>
-    <span  class="font-medium"></span> 
-    {{ messageUser }}
+  <div class="mx-auto">
+    <span class="font-medium text-gray-700 dark:text-gray-400">{{ messageUser }}</span> 
   </div>
 </div>
+
   <div class="w-full">
     <TaskComponent/>
   </div>
@@ -52,7 +50,7 @@ export default {
         // Almacena la instancia del usuario en Vuex
         store.dispatch('setUser', user);
         
-        messageUser.value = `Hola ${user.name}`;
+        messageUser.value = `Bienvenido ${user.name}`;
 
         
       } catch (e) {
